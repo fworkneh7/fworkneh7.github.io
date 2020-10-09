85c424bf362b0250eaf739f8760413a65206c9ec
fworkneh7 <fworkneh@umd.edu>
2020-10-09 16:13:14 -0400

    major javascript/css changes

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
-    res.send('Hello world');
-  })
-  .post((req, res) => {
-    console.log('POST request detected');
-    res.send('hello world'); 
     res.send(`Lab 5 for ${process.env.NAME}`);
   })
   .post((req, res) => {
